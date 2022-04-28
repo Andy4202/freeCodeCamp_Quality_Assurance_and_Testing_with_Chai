78f0f3347ca8c11627156698ab1501e833c73174
Andy Allan <Andy4202@gmail.com>
2022-04-28 14:36:52 +0100

    Challenge 6 complete.

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -68,7 +68,7 @@ suite('Unit Tests', function () {
       assert.notStrictEqual(6, '6');
       assert.strictEqual(6, 3 * 2);
       assert.strictEqual(6 * '2', 12);
-      assert.notstrictEqual([1, 'a', {}], [1, 'a', {}]);
+      assert.notStrictEqual([1, 'a', {}], [1, 'a', {}]);
     });
     // #7
     test('#deepEqual, #notDeepEqual', function () {
