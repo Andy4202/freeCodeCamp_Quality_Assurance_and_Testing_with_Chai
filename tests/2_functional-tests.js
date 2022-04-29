d2ad41f5b3b326c0cdfa09c91c9ccd5e103a5b2d
Andy Allan <Andy4202@gmail.com>
2022-04-29 13:14:43 +0100

    Challenge 21 complete.

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -43,12 +43,11 @@ suite('Functional Tests', function () {
         .send({
           "surname":"Colombo"
         })
-
         .end(function (err, res) {
           assert.equal(res.status, 200);
           assert.equal(res.type, 'application/json');
-          assert.equal(body.name, 'Cristoforo');
-          assert.equal(body.surname, 'Colombo');
+          assert.equal(res.body.name, 'Cristoforo');
+          assert.equal(res.body.surname, 'Colombo');
           done();
         });
     });
